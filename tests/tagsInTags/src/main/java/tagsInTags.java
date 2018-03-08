b44f35d7bebfd60e2963a1b8674a4a778c44c737
jfedoruk <kubafedoruk@gmail.com>
2018-03-08 08:34:51 +0100

    move codeswholesale to bitbucket

---
+++ tests/tagsInTags/src/main/java/tagsInTags.java
@@ -0,0 +1,13 @@
+package java;
+class tagsInTags {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        findTagsInTags();
+    }
+
+    private static void findTagsInTags() {
+    }
+
+}
